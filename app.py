4f446a399d13e2ce741521e67f357ca631b40483
Sawan Kumar Chouhan <56783840+Unity11991@users.noreply.github.com>
2023-01-20 08:31:18 +0530

    code update

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
